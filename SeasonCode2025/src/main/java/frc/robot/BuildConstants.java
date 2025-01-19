2e5497ff02f94daf96f7b169409469ebfa85ec6b
William <149019293+IPutSoapInMyEyes@users.noreply.github.com>
2025-01-18 17:07:29 -0800

    Climb code additions

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "885b4fe5135694b2ecaa67d6c7ebf5fe7da4db03";
-  public static final String GIT_DATE = "2025-01-15 21:21:03 EST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "b23e30cb712aac24b5a6c3b6769c7eaef7b77ab5";
+  public static final String GIT_DATE = "2025-01-18 15:38:00 EST";
   public static final String GIT_BRANCH = "Sn1-2024-vision-testing";
-  public static final String BUILD_DATE = "2025-01-15 21:43:30 EST";
-  public static final long BUILD_UNIX_TIME = 1736995410911L;
+  public static final String BUILD_DATE = "2025-01-18 15:59:14 EST";
+  public static final long BUILD_UNIX_TIME = 1737233954452L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
