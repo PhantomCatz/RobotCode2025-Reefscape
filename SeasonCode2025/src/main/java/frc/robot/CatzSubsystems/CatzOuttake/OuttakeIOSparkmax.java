0eca1578ea341275627b1b87a6733edbb3a0b00a
Kynam L <115427320+Noto501@users.noreply.github.com>
2025-01-23 18:49:48 -0800

    Update OuttakeIOSparkmax.java

--- SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzOuttake/OuttakeIOSparkmax.java
+++ SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzOuttake/OuttakeIOSparkmax.java
@@ -8,8 +8,15 @@
 package frc.robot.CatzSubsystems.CatzOuttake;
 
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SignalsConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 
@@ -25,7 +32,7 @@ public class OuttakeIOSparkmax implements OuttakeIO {
     OuttakeLeftMtr = new SparkMax(1, MotorType.kBrushless);
     OuttakeRightMtr = new SparkMax(2, MotorType.kBrushless);
 
-    // SparkBaseConfig globalConfig = new SparkBaseConfig();
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
 
     globalConfig.smartCurrentLimit(50);
     globalConfig.idleMode(IdleMode.kBrake);
