77e4adb0e0024c88051e6b4455e81e2fea92800c
Kynam L <115427320+Noto501@users.noreply.github.com>
2025-01-29 22:14:50 -0800

    Merge fixes

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "a5920d8b0a2d0ba7432f41fa37236b46848f6394";
-  public static final String GIT_DATE = "2025-01-28 10:44:03 EST";
-  public static final String GIT_BRANCH = "Feature/AlgaeEffector";
-  public static final String BUILD_DATE = "2025-01-29 20:34:18 EST";
-  public static final long BUILD_UNIX_TIME = 1738200858353L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "9d5f022d40bb789785437f19eb801ad25a544fef";
+  public static final String GIT_DATE = "2025-01-29 20:31:13 EST";
+  public static final String GIT_BRANCH = "Feature/outtakePrototype-V1";
+  public static final String BUILD_DATE = "2025-01-30 01:10:48 EST";
+  public static final long BUILD_UNIX_TIME = 1738217448247L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
