968d16ec2f60c1df6633f77cf4d77739aae11e86
William <149019293+IPutSoapInMyEyes@users.noreply.github.com>
2025-02-02 13:49:07 -0800

    Plugin Changes

--- SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzOuttake/OuttakeConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzOuttake/OuttakeConstants.java
@@ -16,7 +16,7 @@ public class OuttakeConstants {
     public static final int OUTTAKE_CURRENT_LIMIT = 30;
 
     public static final int LEFT_OUTTAKE_ID = 21;
-    public static final int RIGHT_OUTTAKE_ID = 20; 
+    public static final int RIGHT_OUTTAKE_ID = 20;
     public static final int FRONT_BEAM_BREAK_ID = 1;
     public static final int BACK_BEAM_BREAK_ID = 0;
 
