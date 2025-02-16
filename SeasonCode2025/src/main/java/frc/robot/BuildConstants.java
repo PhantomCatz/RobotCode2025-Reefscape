bf967386398118cc8c8f19fcc794ae6e3eb68424
Kynam L <115427320+Noto501@users.noreply.github.com>
2025-02-15 21:23:29 -0800

    added gitignore for build constants file

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 205;
-  public static final String GIT_SHA = "f6b2298dfe0548a6b9c9fe2ed63b1fabd6dbf6ce";
-  public static final String GIT_DATE = "2025-02-15 15:35:34 EST";
-  public static final String GIT_BRANCH = "Feature/LED-Implementation";
-  public static final String BUILD_DATE = "2025-02-15 20:00:47 EST";
-  public static final long BUILD_UNIX_TIME = 1739667647067L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 212;
+  public static final String GIT_SHA = "266ab2ce2de97cb9e49bb6d35a4a1a24ff861cfb";
+  public static final String GIT_DATE = "2025-02-16 00:21:11 EST";
+  public static final String GIT_BRANCH = "resolve-doubletap";
+  public static final String BUILD_DATE = "2025-02-16 00:22:51 EST";
+  public static final long BUILD_UNIX_TIME = 1739683371831L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
