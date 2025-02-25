2c4b94796dcef979ca50fee1f9e64d9d4f1a7e3f
William <149019293+IPutSoapInMyEyes@users.noreply.github.com>
2025-02-24 17:03:19 -0800

    2/24 commits

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 255;
-  public static final String GIT_SHA = "ece4a0c8b4b9802011f936c3f085367807822294";
-  public static final String GIT_DATE = "2025-02-23 18:24:39 EST";
+  public static final int GIT_REVISION = 256;
+  public static final String GIT_SHA = "6ff98f4bd61bb3b0f7538abe150f5ab090d46c1f";
+  public static final String GIT_DATE = "2025-02-23 20:13:15 EST";
   public static final String GIT_BRANCH = "da-vinci-testing";
-  public static final String BUILD_DATE = "2025-02-23 20:08:59 EST";
-  public static final long BUILD_UNIX_TIME = 1740359339292L;
+  public static final String BUILD_DATE = "2025-02-24 19:49:03 EST";
+  public static final long BUILD_UNIX_TIME = 1740444543032L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- SeasonCode2025/src/main/java/frc/robot/Robot.java
+++ SeasonCode2025/src/main/java/frc/robot/Robot.java
@@ -18,7 +18,7 @@ import frc.robot.CatzConstants.RobotHardwareMode;
 import frc.robot.CatzConstants.RobotID;
 import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.CatzRobotTracker;
 import frc.robot.CatzSubsystems.CatzLEDs.CatzLED;
-import frc.robot.CatzSubsystems.CatzLEDs.CatzLED.ControllerLEDState;
+import frc.robot.CatzSubsystems.CatzLEDs.CatzLED.railingState;
 import frc.robot.Utilities.Alert;
 import frc.robot.Utilities.Alert.AlertType;
 import frc.robot.Utilities.VirtualSubsystem;
@@ -242,7 +242,7 @@ public class Robot extends LoggedRobot {
               "*** Auto cancelled in %.2f secs ***%n", Timer.getFPGATimestamp() - autoStart);
         }
         autoMessagePrinted = true;
-        CatzLED.getInstance().ledState = ControllerLEDState.autoFinished;
+        CatzLED.getInstance().RailingState = railingState.autoFinished;
         CatzLED.getInstance().autoFinishedTime = Timer.getFPGATimestamp();
       }
     }
@@ -279,7 +279,7 @@ public class Robot extends LoggedRobot {
     if (RobotController.getBatteryVoltage() <= LOW_BATTERY_VOLTAGE
         && DISABLED_TIMER.hasElapsed(LOW_BATTERY_DISABLED_TIME)) {
       LOW_BATTERY_ALERT.set(true);
-      CatzLED.getInstance().ledState = ControllerLEDState.lowBatteryAlert;
+      CatzLED.getInstance().RailingState = railingState.lowBatteryAlert;
     }
   }
 
