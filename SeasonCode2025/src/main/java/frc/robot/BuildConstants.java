6b89d7fc7161ba06aecc71ebd42f4b8d7d0a94f4
YKS44 <79933420+YKS44@users.noreply.github.com>
2025-03-07 09:41:04 -0800

    modify some paths

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 352;
-  public static final String GIT_SHA = "6f05f7c54939f6292b85f4d8beeea12ee2b4ec42";
-  public static final String GIT_DATE = "2025-03-07 01:42:38 EST";
+  public static final int GIT_REVISION = 355;
+  public static final String GIT_SHA = "a0416bd392712877b6670ca06367643e20ec16e2";
+  public static final String GIT_DATE = "2025-03-07 11:07:33 EST";
   public static final String GIT_BRANCH = "driveforwardscore";
-  public static final String BUILD_DATE = "2025-03-07 01:50:58 EST";
-  public static final long BUILD_UNIX_TIME = 1741330258341L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-03-07 12:38:47 EST";
+  public static final long BUILD_UNIX_TIME = 1741369127373L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
