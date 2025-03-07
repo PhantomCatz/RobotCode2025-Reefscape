99c1f7cac446c55d56ac8d4361d7fe053509d675
YKS44 <79933420+YKS44@users.noreply.github.com>
2025-03-06 22:55:20 -0800

    lowered auton speed

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 350;
-  public static final String GIT_SHA = "f1e2ef0c43df896045a2a3ca86312b994a10c290";
-  public static final String GIT_DATE = "2025-03-06 21:02:28 EST";
+  public static final int GIT_REVISION = 352;
+  public static final String GIT_SHA = "6f05f7c54939f6292b85f4d8beeea12ee2b4ec42";
+  public static final String GIT_DATE = "2025-03-07 01:42:38 EST";
   public static final String GIT_BRANCH = "driveforwardscore";
-  public static final String BUILD_DATE = "2025-03-07 01:40:46 EST";
-  public static final long BUILD_UNIX_TIME = 1741329646262L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-07 01:50:58 EST";
+  public static final long BUILD_UNIX_TIME = 1741330258341L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
