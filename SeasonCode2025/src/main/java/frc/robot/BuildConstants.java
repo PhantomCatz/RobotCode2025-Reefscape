c7f552f917e6b55b0483315e5cb00bbb9754d71e
Kynam L <colintarng@yahoo.com>
2025-03-18 17:54:36 -0700

    did it

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 372;
-  public static final String GIT_SHA = "a0ac00678704c3c064ba3a802709c3a2b1b166fa";
-  public static final String GIT_DATE = "2025-03-17 17:37:29 EDT";
-  public static final String GIT_BRANCH = "constant-tracking-score";
-  public static final String BUILD_DATE = "2025-03-17 19:29:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1742254143093L;
-
+  public static final int GIT_REVISION = 398;
+  public static final String GIT_SHA = "1a70539ab6110703ba8a8bd298ffcf3aa5f2f11a";
+  public static final String GIT_DATE = "2025-03-18 20:37:38 EDT";
+  public static final String GIT_BRANCH = "heading-change-2";
+  public static final String BUILD_DATE = "2025-03-18 20:53:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1742345639154L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
