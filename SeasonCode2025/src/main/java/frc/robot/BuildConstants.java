e18fe379e6ebe2916b625afb37b3cf869f8bdddb
Kynam L <115427320+Noto501@users.noreply.github.com>
2025-03-22 08:12:07 -0700

    voltage control addon

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 406;
-  public static final String GIT_SHA = "c6d51798cf891e71fad24b761a9e140d395eab6a";
-  public static final String GIT_DATE = "2025-03-20 19:56:53 EDT";
+  public static final int GIT_REVISION = 407;
+  public static final String GIT_SHA = "fcdfffc6c9372f41053ffdd114ef780085a05735";
+  public static final String GIT_DATE = "2025-03-22 00:25:53 EDT";
   public static final String GIT_BRANCH = "tune-autoaim";
-  public static final String BUILD_DATE = "2025-03-22 00:23:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1742617409532L;
+  public static final String BUILD_DATE = "2025-03-22 02:47:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1742626031645L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
