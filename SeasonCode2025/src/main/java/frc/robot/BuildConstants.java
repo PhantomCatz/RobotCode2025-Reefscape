75a7d6c6b3f784450cbb355809200a9a71e5e76d
William <149019293+IPutSoapInMyEyes@users.noreply.github.com>
2025-04-02 18:38:42 -0700

    ramp angle goold

--- SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
+++ SeasonCode2025/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "SeasonCode2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 450;
-  public static final String GIT_SHA = "a26794b23bac8c7742854829677673b870f41ccb";
-  public static final String GIT_DATE = "2025-04-02 03:09:15 EDT";
+  public static final int GIT_REVISION = 451;
+  public static final String GIT_SHA = "8aa7b431fa61830e888a505b095ce001acb50ad5";
+  public static final String GIT_DATE = "2025-04-02 13:10:44 EDT";
   public static final String GIT_BRANCH = "preaerospace-davinci";
-  public static final String BUILD_DATE = "2025-04-02 12:54:29 EDT";
-  public static final long BUILD_UNIX_TIME = 1743612869395L;
+  public static final String BUILD_DATE = "2025-04-02 21:33:06 EDT";
+  public static final long BUILD_UNIX_TIME = 1743643986759L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
