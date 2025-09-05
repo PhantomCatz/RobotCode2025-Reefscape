740f8564abddb969cd1b6f50c6aceb382395e4c7
YKS44 <79933420+YKS44@users.noreply.github.com>
2025-09-04 22:48:12 -0700

    implemented limelight helpers

--- SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzDriveAndRobotOrientation/Vision/VisionIOLimelight.java
+++ SeasonCode2025/src/main/java/frc/robot/CatzSubsystems/CatzDriveAndRobotOrientation/Vision/VisionIOLimelight.java
@@ -1,3 +1,4 @@
+// VisionIOLimelight.java
 package frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.Vision;
 
 import edu.wpi.first.math.geometry.Pose3d;
@@ -5,13 +6,9 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.DoubleArrayPublisher;
-import edu.wpi.first.networktables.DoubleArraySubscriber;
-import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Timer;
-import frc.robot.FieldConstants;
 import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.CatzRobotTracker;
+import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.Vision.LimelightHelpers.LimelightResults;
+import frc.robot.CatzSubsystems.CatzDriveAndRobotOrientation.Vision.LimelightHelpers.LimelightTarget_Fiducial;
 
 
 import java.util.HashSet;
@@ -20,192 +17,102 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
-import org.littletonrobotics.junction.Logger;
 
-/** IO implementation for real Limelight hardware. */
+/** IO implementation for real Limelight hardware using LimelightHelpers. */
 public class VisionIOLimelight implements VisionIO {
   private final Supplier<Rotation2d> rotationSupplier = () -> CatzRobotTracker.Instance.getEstimatedPose().getRotation();
-  private final DoubleArrayPublisher orientationPublisher;
-
-  // private final SwerveDrivePoseEstimator
-
-  private final DoubleSubscriber latencySubscriber;
-  private final DoubleSubscriber txSubscriber;
-  private final DoubleSubscriber taSubscriber;
-
-  private final DoubleSubscriber tySubscriber;
-  private final DoubleSubscriber tagIDSubscriber;
-  private final DoubleArraySubscriber cameraSpaceSubscriber;
-  private final DoubleArraySubscriber megatag1Subscriber;
-  private final DoubleArraySubscriber megatag2Subscriber;
-
+  
   public final String name;
 
-  /**
-   * Creates a new VisionIOLimelight.
-   *
-   * @param name The configured name of the Limelight.
-   * @param rotationSupplier Supplier for the current estimated rotation, used for MegaTag 2.
-   */
   public VisionIOLimelight(String name) {
     this.name = name;
-    var table            = NetworkTableInstance.getDefault().getTable(name);
-    orientationPublisher = table.getDoubleArrayTopic("robot_orientation_set").publish();
-    latencySubscriber    = table.getDoubleTopic("tl").subscribe(0.0);
-    txSubscriber         = table.getDoubleTopic("tx").subscribe(0.0);
-    tySubscriber         = table.getDoubleTopic("ty").subscribe(0.0);
-    taSubscriber         = table.getDoubleTopic("ta").subscribe(0.0);
-
-    tagIDSubscriber      = table.getDoubleTopic("tid").subscribe(0.0);
-    cameraSpaceSubscriber= table.getDoubleArrayTopic("targetpose_cameraspace").subscribe(new double[] {});
-    megatag1Subscriber   = table.getDoubleArrayTopic("botpose_wpiblue").subscribe(new double[] {});
-    megatag2Subscriber   = table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(new double[] {});
   }
 
   @Override
   public void updateInputs(VisionIOInputs inputs) {
-    double usedTimestamp = Timer.getFPGATimestamp();
-    // Update connection status based on whether an update has been seen in the last 250ms
-    inputs.connected = (usedTimestamp - latencySubscriber.getLastChange()) < 250;
+    LimelightResults results = LimelightHelpers.getLatestResults(name);
+    inputs.connected = results.valid;
 
-    // Update orientation for MegaTag 2
-    orientationPublisher.accept(new double[] {rotationSupplier.get().getDegrees(), 0.0, 0.0, 0.0, 0.0, 0.0}); //TODO possible sync error
+    LimelightHelpers.SetRobotOrientation(name, rotationSupplier.get().getDegrees(), 0.0, 0.0, 0, 0, 0);
 
-    inputs.ta = taSubscriber.get();
-    // Read new pose observations from NetworkTables
+    if (!inputs.connected) {
+      inputs.latestTargetObservations = new TargetObservation[0];
+      inputs.poseObservations = new PoseObservation[0];
+      inputs.tagIds = new int[0];
+      inputs.ta = 0.0;
+      return;
+    }
+
+    inputs.ta = results.botpose_avgarea;
+    
     Set<Integer> tagIds = new HashSet<>();
     List<PoseObservation> poseObservations = new LinkedList<>();
     List<TargetObservation> targetObservations = new LinkedList<>();
+    
+    LimelightTarget_Fiducial[] fiducials = results.targets_Fiducials;
 
-    //----------------------------------------------------------------------------------------------
-    // Single tag Pose estimation
-    //----------------------------------------------------------------------------------------------
-    for (var rawSample : cameraSpaceSubscriber.readQueue()) {
-      if (rawSample.value.length == 0) continue;
-      Translation3d cameraSpaceTranslation = new Translation3d(
-        rawSample.value[0],
-        rawSample.value[1],
-        rawSample.value[2]
-        );
-        targetObservations.add(
+    // --- Pre-calculate aggregate data from all visible fiducials ---
+    int tagCount = fiducials.length;
+    double totalDistance = 0.0;
+    double maxAmbiguity = 0.0;
+    
+    for (LimelightTarget_Fiducial target : fiducials) {
+      tagIds.add((int) target.fiducialID);
+      Translation3d cameraSpaceTranslation = target.getTargetPose_CameraSpace().getTranslation();
+      totalDistance += cameraSpaceTranslation.getNorm();
+      maxAmbiguity = Math.max(maxAmbiguity, 0.0);
+      
+      targetObservations.add(
           new TargetObservation(
-            usedTimestamp,
-            rawSample.value[0],
-            rawSample.value[1],
-            rawSample.value[2],
-            (int) tagIDSubscriber.get(),
+            results.timestamp_RIOFPGA_capture,
+            target.tx,
+            target.ty,
+            0.0,
+            (int) target.fiducialID,
             cameraSpaceTranslation.getNorm())
-            );
-          }
-          try{
-            for(var rawSample :megatag1Subscriber.readQueue()){
-              if(rawSample.value.length == 0) continue;
-              Logger.recordOutput("Megatag1Pose", parsePose(rawSample.value));
+      );
+    }
+    double avgTagDistance = (tagCount > 0) ? totalDistance / tagCount : 0;
 
-            }
-          }catch (Exception e){
-            e.printStackTrace();
-          }
 
-          //----------------------------------------------------------------------------------------------
-          // Megatag 1 estimation
-          //----------------------------------------------------------------------------------------------
-          // for (var rawSample : megatag1Subscriber.readQueue()) {
-          //   // if sample is invalid, skip
-          //   if (rawSample.value.length == 0) continue;
-          //   poseObservations.add(
-          //     new PoseObservation(
-          //       // Timestamp, based on server timestamp of publish and latency
-          //       usedTimestamp - rawSample.value[6] * 1.0e-3,
-
-          //       // 3D pose estimate
-          //       parsePose(rawSample.value),
-
-          //       // Ambiguity, zeroed because the pose is already disambiguated
-          //     rawSample.value[8],
-
-          //     // Tag count
-          //     (int) rawSample.value[7],
-
-          //     // Average tag distance
-          //     rawSample.value[9], // Used to grab area at value 10 //TODO
-
-          //     // Observation type
-          //     PoseObservationType.MEGATAG_1)
-          //     );
-          //   }
-
-            NetworkTableInstance.getDefault().flush(); // Increases network traffic but recommended by Limelight
-
-    // System.out.println("lastchange: " + megatag2Subscriber.getLastChange() * 1e-6);
-    // System.out.println("cur: " + usedTimestamp);
+    //----------------------------------------------------------------------------------------------
+    // Megatag 1 estimation
+    //----------------------------------------------------------------------------------------------
+    Pose3d botpose_mega1 = LimelightHelpers.getBotPose3d_wpiBlue(name);
+    if (!botpose_mega1.equals(new Pose3d())) { // Check if the pose is valid (not zeroed out)
+        poseObservations.add(
+            new PoseObservation(
+                results.timestamp_RIOFPGA_capture,
+                botpose_mega1, // Use the Pose3d object directly
+                maxAmbiguity,  // Use calculated max ambiguity
+                tagCount,      // Use calculated tag count
+                avgTagDistance,// Use calculated average distance
+                PoseObservationType.MEGATAG_1)
+        );
+    }
+    
     //----------------------------------------------------------------------------------------------
     // Megatag 2 estimation
     //----------------------------------------------------------------------------------------------
-    for (var rawSample : megatag2Subscriber.readQueue()) {
-      // System.out.println("latency: " + rawSample.value[6]);
-      // if sample is invalid, skip
-      if (rawSample.value.length == 0) continue;
-      poseObservations.add(
-          new PoseObservation(
-              // Timestamp, based on server timestamp of publish and latency
-              usedTimestamp - ((rawSample.value[6] + VisionConstants.PING_MAP.get(name)) * 1.0e-3),
-
-              // 3D pose estimate
-              parsePose(rawSample.value),
-
-              // Ambiguity, zeroed because the pose is already disambiguated
-              0.0,
-
-              // Tag count
-              (int) rawSample.value[7],
-
-              // Average tag distance
-              rawSample.value[9], // Used to grab area at value 10 //TODO
-
-              // Observation type
-              PoseObservationType.MEGATAG_2)
-      );
+    Pose3d botpose_mega2 = LimelightHelpers.getBotPose3d_wpiBlue(name);
+    if (!botpose_mega2.equals(new Pose3d())) { // Check if the pose is valid
+        poseObservations.add(
+            new PoseObservation(
+                results.timestamp_RIOFPGA_capture,
+                botpose_mega2, // Use the Pose3d object directly
+                0.0,           // Ambiguity is zeroed, as MegaTag2 is fully disambiguated
+                tagCount,      // Use calculated tag count
+                avgTagDistance,// Use calculated average distance
+                PoseObservationType.MEGATAG_2)
+        );
     }
 
-    // Save pose observations to inputs object
-    inputs.poseObservations = new PoseObservation[poseObservations.size()];
-    for (int i = 0; i < poseObservations.size(); i++) {
-      inputs.poseObservations[i] = poseObservations.get(i);
-    }
-
-    // Save Tx Ty observation to inputs object
-    inputs.latestTargetObservations = new TargetObservation[targetObservations.size()];
-    for (int i = 0; i < targetObservations.size(); i++) {
-      inputs.latestTargetObservations[i] = targetObservations.get(i);
-    }
-
-    // Save tag IDs to inputs objects
-
-    if(tagIDSubscriber.exists() && tagIDSubscriber.get() >= 1 && tagIDSubscriber.get() <= FieldConstants.APRILTAG_LAYOUT.getTags().size()){
-      tagIds.add((int) tagIDSubscriber.get());
-    }
-
-
-
-    int i = 0;
-    inputs.tagIds = new int[tagIds.size()];
-    for (int id : tagIds) {
-      inputs.tagIds[i++] = id;
-    }
-
+    inputs.poseObservations = poseObservations.toArray(new PoseObservation[0]);
+    inputs.latestTargetObservations = targetObservations.toArray(new TargetObservation[0]);
+    inputs.tagIds = tagIds.stream().mapToInt(Integer::intValue).toArray();
     inputs.name = name;
   }
 
-  /** Parses the 3D pose from a Limelight botpose array. */
-  private static Pose3d parsePose(double[] rawLLArray) {
-    return new Pose3d(
-        rawLLArray[0],
-        rawLLArray[1],
-        rawLLArray[2],
-        new Rotation3d(
-            Units.degreesToRadians(rawLLArray[3]),
-            Units.degreesToRadians(rawLLArray[4]),
-            Units.degreesToRadians(rawLLArray[5])));
-  }
+  // The parsePose method is no longer needed with the new LimelightHelpers API.
+  // private static Pose3d parsePose(double[] rawLLArray) { ... }
 }
